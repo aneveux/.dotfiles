52070a9b12ec86117d14a8b114b0306e454b0683
Antoine Neveux <aneveux@cloudbees.com>
2024-03-27 15:20:14 +0100

    updates

--- dot_i3/executable_laptop.sh
+++ dot_i3/executable_laptop.sh
@@ -4,7 +4,7 @@ monitors=$(xrandr --query | grep " connected " | cut -d" " -f1)
 
 for monitor in $monitors; do
   if [[ "$monitor" =~ ^eDP.* ]]; then
-    xrandr --output "$monitor" --primary --mode 1920x1080 --rotate normal
+    xrandr --output "$monitor" --primary --mode 1920x1200 --rotate normal
   else
     xrandr --output "$monitor" --off
   fi
