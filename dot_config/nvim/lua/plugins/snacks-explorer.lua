69d868a699336e4f14ee40b4f54b08c407230c58
Antoine Neveux <aneveux@cloudbees.com>
2025-02-11 16:29:59 +0100

    Enhance explorer navigation and refactor custom git command
    1. **Enhance Snacks Explorer Navigation:**
       - The function responsible for focusing on the Explorer now includes a
         delay mechanism to prevent accidental focus on the explorer's
         textbox. By leveraging `vim.defer_fn` and feeding the escape key
         after a brief delay, the escape mechanism is triggered, ensuring
         seamless navigation within the Snacks Explorer.
       - This improvement eliminates potential disruptions in user workflow,
         improving overall user experience when interacting with the Explorer
         in Neovim.
    
    2. **Refactor Git Command in Zsh Configuration:**
       - An obsolete alias (`gcai`) in the `dot_aliases` file was removed
         to streamline configurations and prevent redundancy.
       - A new `commit` function is introduced in `dot_zshrc.d/git.zsh` to
         facilitate creating commit messages inline. This function prepares a
         commit message draft by capturing the differences and processes it
         with `mods`, conveniently storing the output in a temporary
         `.git/gcommit` file before enabling final editing and committing.

--- dot_config/nvim/lua/plugins/snacks-explorer.lua
+++ dot_config/nvim/lua/plugins/snacks-explorer.lua
@@ -6,7 +6,16 @@ return {
       {
         "<leader>e",
         function()
-          Snacks.picker.get({ source = "explorer" })[1].input.win:focus()
+          local explorer = Snacks.picker.get({ source = "explorer" })[1]
+          if explorer then
+            explorer.input.win:focus()
+            -- I don't want the explorer textbox to be focused
+            vim.defer_fn(function()
+              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+            end, 50) -- Small delay to ensure it escapes properly
+          else
+            Snacks.explorer({ cwd = LazyVim.root() })
+          end
         end,
         desc = "Focus Snacks Explorer",
       },
--- dot_zshrc.d/git.zsh
+++ dot_zshrc.d/git.zsh
@@ -1,2 +1,7 @@
 alias fork='gh repo fork --remote-name aneveux --remote'
 function ignore() { curl -L -s https://www.gitignore.io/api/\$@ ;}
+
+function commit() {
+  git --no-pager diff | mods 'write a commit message for this patch. also write the long commit message. break the lines at 80 chars' > .git/gcommit
+  git commit -a -F .git/gcommit -e
+}
