d3567811879576f0f27d53a1a1264994afc90351
Antoine Neveux <aneveux@cloudbees.com>
2026-04-13 14:18:38 +0200

    Update .zshrc.d/git.zsh

--- dot_zshrc.d/git.zsh
+++ dot_zshrc.d/git.zsh
@@ -1,14 +1,1 @@
-alias fork='gh repo fork --remote-name aneveux --remote'
 function ignore() { curl -L -s https://www.gitignore.io/api/\$@ ;}
-
-function commit() {
-  PROMPT_FILE=~/prompts/git-commit.prompt
-
-  if [[ ! -f "$PROMPT_FILE" ]]; then
-    echo "Error: Prompt file not found at $PROMPT_FILE"
-    return 1
-  fi
-
-  git --no-pager diff --staged HEAD | mods $(cat "$PROMPT_FILE") > .git/gcommit
-  git commit -F .git/gcommit -e
-}
