fee0aef16aeb32e175fd6d79c5ffa8a12c81456d
Antoine Neveux <aneveux@cloudbees.com>
2026-06-17 16:10:01 +0200

    Update .config/gtk-3.0/settings.ini Update .config/gtk-4.0/settings.ini Update .xprofile Update .zshrc.d/sesh.zsh

--- dot_zshrc.d/sesh.zsh
+++ dot_zshrc.d/sesh.zsh
@@ -1,13 +1,6 @@
 function sesh-sessions() {
-  {
-    exec </dev/tty
-    exec <&1
-    local session
-    session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
-    zle reset-prompt > /dev/null 2>&1 || true
-    [[ -z "$session" ]] && return
-    sesh connect $session
-  }
+  BUFFER="tv sesh"
+  zle accept-line
 }
 
 zle     -N             sesh-sessions
