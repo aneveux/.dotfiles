2bb087596bafcf11ced515f3e3b411701276f4e9
Antoine Neveux <aneveux@cloudbees.com>
2026-06-17 16:16:20 +0200

    Update .zshrc.d/sesh.zsh

--- dot_zshrc.d/sesh.zsh
+++ dot_zshrc.d/sesh.zsh
@@ -3,6 +3,11 @@ function sesh-sessions() {
   zle accept-line
 }
 
+_sesh_preexec() {
+  [[ "$1" == "tv sesh" ]] && printf '\e[2K\e[1A\e[2K\r' >/dev/tty
+}
+preexec_functions+=(_sesh_preexec)
+
 zle     -N             sesh-sessions
 zvm_bindkey viins '^o' sesh-sessions
 zvm_bindkey vicmd '^o' sesh-sessions
